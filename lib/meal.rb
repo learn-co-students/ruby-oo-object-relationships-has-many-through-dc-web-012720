8a2a92352b15770c2785817cc3de31f817cda55b
alanbanks229 <alanbanks229@gmail.com>
2020-01-30 15:54:23 -0500

    the lab

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
 
+    attr_reader :name, :age
+    @@all = []
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = name
+        @customer = age
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all()
+        @@all
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,18 @@
 class Waiter
 
+    @@all = []
+    attr_reader :name, :years_experience
+    def initialize(name, years_experience)
+        @name = name
+        @experience = years_experience
+        @@all << self
+    end
+
+    def self.all()
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
 end

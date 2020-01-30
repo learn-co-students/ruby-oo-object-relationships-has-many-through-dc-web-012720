b8a937f1757546a1464af9105f0c977be5ba67ca
matteoaricci <matteo.a.ricci@gmail.com>
2020-01-30 12:12:09 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require_relative './meal.rb'
+require_relative './waiter.rb'
+require 'pry'
+
 class Customer
-  
+  attr_reader :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.find_all do |meals| meals.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meals|
+        meals.waiter
+    end
+  end
+
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+    attr_reader :customer, :total, :tip
+    attr_accessor :waiter
 
+    @@all = []
+
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
